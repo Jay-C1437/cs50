c6c39270d942dd73b121d550b6626dffc9ddc986
John Chris Matovu <jay427002@gmail.com>
2025-01-21 12:27:37 +0300

    defining list of names
    I was trying out on I should be defining list in python

---
+++ list.py
@@ -0,0 +1,15 @@
+#define a list of names 
+names =["Alice", "Bob", "Charlie"]
+
+# print the list of names
+# print(names)
+# print(names[0])
+# print(names[1])
+# print(names[2])
+
+
+names.append("David") #this will add a name to the list
+
+names.sort()#this will automatically sort the names in alphabetical order
+
+print(names)
