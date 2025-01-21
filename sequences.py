44d0779ffbc16796085648a2d96e3b29f36e8ac8
John Chris Matovu <jay427002@gmail.com>
2025-01-21 10:36:23 +0300

    sequences.py
    store multiple items together and access them using their position (index).

---
+++ sequences.py
@@ -0,0 +1,13 @@
+# name = "Jayc"
+
+# print(name[1])
+
+# names = ["Alice", "Bob", "Charlie"]
+# print(names)
+# print(names[0])
+
+
+coordinateX = 10.0
+coordinateY = 20.0
+
+coordinate = (10.0, 20.0)
