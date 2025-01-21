2e8ff13f07c127544879da41e2fe5b18131e929b
John Chris Matovu <jay427002@gmail.com>
2025-01-21 12:37:32 +0300

    Sets in python
    getting to understand the basics of sets in python

---
+++ set.py
@@ -0,0 +1,14 @@
+#create an empty set
+s = set()
+
+#add elements to the set
+s.add(1)
+s.add(2)
+s.add(3)
+s.add(4)
+
+
+s.remove(2) #this will remove the element 2 from the set
+print(s)
+
+print(f"The set has{len(s)}elements.") #this will print the number of elements in the set
