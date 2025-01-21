ef5bf2f8424d910209217005b7c0af8cf86a24ac
John Chris Matovu <jay427002@gmail.com>
2025-01-21 12:54:36 +0300

    loops in python
    this helped me to understand on how I should use loops in python

---
+++ loops.py
@@ -0,0 +1,15 @@
+for i in[0, 1, 2, 3, 4, 5]: #this will print the numbers from 0 to 5
+    print(i)
+
+for i in range(6): #this will print the numbers from 0 to 5
+    print(i)
+
+names = ["Jayc","Jade","Jadee"]
+
+for name in names:
+    print(name)
+
+name = "Jayc"
+
+for character in name:
+    print (character)
